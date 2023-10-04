8273762a39d8304e45d32f2d8713241fa845634d
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2023-10-04 19:04:14 +0200

    SpringBoot upload
    Uploading the creation of the project empty.

---
+++ src/test/java/pet/guardian/PetGuardian/PetGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.guardian.PetGuardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
