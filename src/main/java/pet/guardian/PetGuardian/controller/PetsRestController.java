9a2259273f9f7a28ec6a93468a752dad77ec6d06
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2023-10-09 11:17:05 +0200

    CRUDS
    Simple CRUD of Vet and Pet done

--- src/main/java/pet/guardian/PetGuardian/controller/PetsRestController.java
+++ src/main/java/pet/guardian/PetGuardian/controller/PetsRestController.java
@@ -1,11 +1,43 @@
 package pet.guardian.PetGuardian.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pet.guardian.PetGuardian.model.Pets;
+import pet.guardian.PetGuardian.service.api.PetsServiceAPI;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pets")
 @CrossOrigin("*")
 public class PetsRestController {
+    @Autowired
+    private PetsServiceAPI petsServiceAPI;
+
+    @GetMapping(value = "/all")
+    public List<Pets> getAllPets() throws Exception{
+        return petsServiceAPI.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Pets findPet(@PathVariable String id) throws Exception{
+        return petsServiceAPI.get(id);
+    }
+
+    @PostMapping
+    public void createPet(@RequestBody Pets pet) throws Exception{
+        petsServiceAPI.save(pet);
+    }
+
+    @PutMapping("/{id}")
+    public void updatePet(@PathVariable String id, @RequestBody Pets pet) throws Exception {
+        petsServiceAPI.save(pet, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePet(@PathVariable String id) throws Exception {
+        petsServiceAPI.delete(id);
+    }
+
+
 }
--- src/main/java/pet/guardian/PetGuardian/controller/VetRestController.java
+++ src/main/java/pet/guardian/PetGuardian/controller/VetRestController.java
@@ -1,11 +1,42 @@
 package pet.guardian.PetGuardian.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pet.guardian.PetGuardian.model.Vet;
+import pet.guardian.PetGuardian.service.api.VetServiceAPI;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/vet")
 @CrossOrigin("*")
 public class VetRestController {
+    @Autowired
+    private VetServiceAPI vetServiceAPI;
+
+    @GetMapping(value = "/all")
+    public List<Vet> getAll() throws Exception {
+        return vetServiceAPI.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Vet findVet(@PathVariable String id) throws Exception {
+        return vetServiceAPI.get(id);
+    }
+
+    @PostMapping
+    public void saveVet(@RequestBody Vet vet) throws Exception {
+        vetServiceAPI.save(vet);
+    }
+
+    @PutMapping("/{id}")
+    public void updatePet(@PathVariable String id, @RequestBody Vet vet) throws Exception {
+        vetServiceAPI.save(vet, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVet(@PathVariable String id) throws Exception {
+        vetServiceAPI.delete(id);
+    }
+
 }
