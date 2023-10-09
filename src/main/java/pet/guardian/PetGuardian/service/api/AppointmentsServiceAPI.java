6ff9f3892451b9522435009a86df83078092abcd
JoelAmpurda <ampurda98@gmail.com>
2023-10-09 19:30:03 +0200

    Created the DTOs files for client and appoinment

--- src/main/java/pet/guardian/PetGuardian/service/api/AppointmentsServiceAPI.java
+++ src/main/java/pet/guardian/PetGuardian/service/api/AppointmentsServiceAPI.java
@@ -1,8 +1,9 @@
 package pet.guardian.PetGuardian.service.api;
 
 import pet.guardian.PetGuardian.commons.GenericServiceAPI;
+import pet.guardian.PetGuardian.dto.AppointmentsDTO;
 import pet.guardian.PetGuardian.model.Appointments;
 
-public interface AppointmentsServiceAPI extends GenericServiceAPI<Appointments, Appointments> {
+public interface AppointmentsServiceAPI extends GenericServiceAPI<Appointments, AppointmentsDTO> {
     
 }
