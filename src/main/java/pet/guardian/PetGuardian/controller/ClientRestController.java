3bf43ebc714afde48868d32f7a565bc54e4a1fa5
JoelAmpurda <ampurda98@gmail.com>
2023-10-17 18:26:08 +0200

    Implemented custom endpoints

--- src/main/java/pet/guardian/PetGuardian/controller/ClientRestController.java
+++ src/main/java/pet/guardian/PetGuardian/controller/ClientRestController.java
@@ -1,16 +1,28 @@
 package pet.guardian.PetGuardian.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pet.guardian.PetGuardian.dto.AppointmentsDTO;
 import pet.guardian.PetGuardian.dto.PetDTO;
 import pet.guardian.PetGuardian.model.Client;
+import pet.guardian.PetGuardian.service.api.AppointmentsServiceAPI;
 import pet.guardian.PetGuardian.service.api.ClientServiceAPI;
 import pet.guardian.PetGuardian.service.api.PetServiceAPI;
-
-import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -21,6 +33,9 @@ public class ClientRestController {
     private ClientServiceAPI clientServiceAPI;
     @Autowired
     private PetServiceAPI petServiceAPI;
+    @Autowired
+    private AppointmentsServiceAPI appointmentsServiceAPI;
+
     @GetMapping(value = "/all")
     public ResponseEntity<Object> getAllClient() throws Exception {
         return new ResponseEntity<>(clientServiceAPI.getAll(), HttpStatus.OK);
@@ -30,6 +45,46 @@ public class ClientRestController {
     public ResponseEntity<Object> getClient(@PathVariable String id) throws Exception {
         return new ResponseEntity<>(clientServiceAPI.get(id), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/findPets/{id}")
+    public ResponseEntity<Object> getPets(@PathVariable String id) throws Exception {
+        List<PetDTO> client_pets = new ArrayList<>();
+        List<PetDTO> all_pets = petServiceAPI.getAll();
+        for (PetDTO pet : all_pets) {
+            if (Objects.equals(pet.getClient_id(), id)) {
+                client_pets.add(pet);
+            }
+        }
+        return new ResponseEntity<>(client_pets, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/findAppointments/{id}")
+    public ResponseEntity<Object> getAppointments(@PathVariable String id) throws Exception{
+        List<AppointmentsDTO> all_appointments = appointmentsServiceAPI.getAll();
+        List<AppointmentsDTO> client_appointments = new ArrayList<>();
+        List<PetDTO> all_client_pets = getAllPets(id);
+        for (PetDTO pet : all_client_pets) {
+            for (AppointmentsDTO appoint : all_appointments){
+                if (Objects.equals(pet.getId(), appoint.getPet_id())) {
+                    client_appointments.add(appoint);
+                }
+
+            }
+        }
+        return new ResponseEntity<Object>(client_appointments, HttpStatus.OK);
+    }
+
+    public List<PetDTO> getAllPets(String id) throws Exception {
+        List<PetDTO> client_pets = new ArrayList<>();
+        List<PetDTO> all_pets = petServiceAPI.getAll();
+        for (PetDTO pet : all_pets) {
+            if (Objects.equals(pet.getClient_id(), id)) {
+                client_pets.add(pet);
+            }
+        }
+        return client_pets;
+    }
+
 
     @PostMapping(value = "/create/{id}")
     public ResponseEntity<String> createClient(@RequestBody Client client, @PathVariable String id) throws Exception {
@@ -50,7 +105,7 @@ public class ClientRestController {
             if (Objects.equals(pet.getClient_id(), id)) {
                 try {
                     pet.setClient_id("null");
-                    petServiceAPI.save(pet,pet.getId());
+                    petServiceAPI.save(pet, pet.getId());
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -60,14 +115,14 @@ public class ClientRestController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    private Client patchClient(Client clientToUpdate, Client client){
-        if (client.getName() != null){
+    private Client patchClient(Client clientToUpdate, Client client) {
+        if (client.getName() != null) {
             clientToUpdate.setName(client.getName());
         }
-        if (client.getPhone() != null){
+        if (client.getPhone() != null) {
             clientToUpdate.setPhone(client.getPhone());
         }
-        if (client.getEmail()!= null)
+        if (client.getEmail() != null)
             clientToUpdate.setEmail(client.getEmail());
         if (client.getSurnames() != null)
             clientToUpdate.setSurnames(client.getSurnames());
